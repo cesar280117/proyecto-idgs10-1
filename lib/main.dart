2c32df15f74b0ea406c7b28c6460722cd9d20e61
Cesar <siegithub@utslrc.edu.mx>
2022-02-16 16:48:52 -0800

    se agrego la seccion de examen1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:contador/controllers/contador_controller.dart';
 import 'package:contador/pages/card_page.dart';
 import 'package:contador/pages/contador_page.dart';
 import 'package:contador/pages/container_pages.dart';
+import 'package:contador/pages/examen1_page.dart';
 import 'package:contador/pages/list_view_page.dart';
 import 'package:contador/pages/menu_page.dart';
 import 'package:contador/pages/stack_pages.dart';
@@ -20,14 +21,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Grupo IDGS10-1',
-        initialRoute: '/menu',
+        initialRoute: '/examen1',
         routes: {
           '/menu': (context) => const MenuPage(),
           '/contador': (context) => ContadorPage(),
           '/container': (context) => const ContainerPages(),
           '/cartas': (context) => const CardPage(),
           '/stack': (context) => const StackPages(),
-          '/listView': (context) => const ListViewPage()
+          '/listView': (context) => const ListViewPage(),
+          '/examen1': (context) => const Examen1Pages()
         });
   }
 }
